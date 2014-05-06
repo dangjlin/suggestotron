ef3545ec590a07dc96f4805f897dcc51d59645b9
Daniel K. Lin <yettie.lin@gmail.com>
2014-05-07 01:29:14 +0800

    the first commit of railsbridge project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Suggestotron::Application.config.secret_key_base = '594e825c33de244c629659494a2639273f625b07252dffd46064977fbb43c9904213e93b8343a540740a2aebb0de400ff2d47126b5667105253879bd8efbc7c6'
